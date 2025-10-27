8f0d7d3de0dac4be099921ed2c16be2180902c17
David Sliren <bluee.97.berry@gmail.com>
2025-10-27 16:41:01 -0500

    fix: arreglar errores de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import {BrowserRouter, Routes, Route} from 'react-router';
-import { useState } from 'react'
-import './App.css'
-import Home from './assets/pages/Home';
-import Game from './assets/pages/Game';
+import { BrowserRouter, Routes, Route } from "react-router";
+import { useState } from "react";
+import "./App.css";
+import Home from "./assets/pages/Home";
+import Game from "./assets/pages/Game";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/Memorama2">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/game' element={<Game/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
